d7f8c230135ad1ffac0e4062aebc288c39696883
Anwesh43 <anweshthecool0@gmail.com>
2020-05-17 03:08:53 +0530

    adding Canvas extension method

--- rlbbview/src/main/java/com/anwesh/uiprojects/rlbbview/RotatingLineBallBearerView.kt
+++ rlbbview/src/main/java/com/anwesh/uiprojects/rlbbview/RotatingLineBallBearerView.kt
@@ -19,12 +19,39 @@ val sizeFactor : Float = 2.9f
 val delay : Long = 20
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 2
+val rFactor : Float = 2.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawRotatingLineBall(scale : Float, size : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val r : Float = size / rFactor
+    save()
+    rotate(90f * sf1)
+    drawLine(0f, 0f, 0f, 2 * size, paint)
+    drawCircle(size, -r  - (h / 2 - 2 * r) * sf2, r, paint)
+    restore()
+}
+
+fun Canvas.drawRLBBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawRotatingLineBall(scale, size, h, paint)
+    restore()
+}
 
 class RotatingLineBallBeaterView(ctx : Context) : View(ctx) {
 

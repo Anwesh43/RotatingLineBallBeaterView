fd66c6f34c24c9608ca7624a8e16567c333b880c
Anwesh43 <anweshthecool0@gmail.com>
2020-05-17 03:20:00 +0530

    adding RotatingLineBall class

--- rlbbview/src/main/java/com/anwesh/uiprojects/rlbbview/RotatingLineBallBearerView.kt
+++ rlbbview/src/main/java/com/anwesh/uiprojects/rlbbview/RotatingLineBallBearerView.kt
@@ -157,4 +157,28 @@ class RotatingLineBallBeaterView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotatingLineBall(var i : Int) {
+
+        private val root : RLBBNode = RLBBNode(0)
+        private var curr : RLBBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                    cb(it)
+                }
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

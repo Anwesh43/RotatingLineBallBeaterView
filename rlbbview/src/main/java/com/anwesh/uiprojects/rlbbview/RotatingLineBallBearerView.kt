4f45d9546462a7bc3498ec0c2efd4c4a135f9a19
Anwesh43 <anweshthecool0@gmail.com>
2020-05-17 03:00:49 +0530

    adding RotatingLineBallBeaterView class and importing libraries

--- rlbbview/src/main/java/com/anwesh/uiprojects/rlbbview/RotatingLineBallBearerView.kt
+++ rlbbview/src/main/java/com/anwesh/uiprojects/rlbbview/RotatingLineBallBearerView.kt
@@ -5,6 +5,12 @@ package com.anwesh.uiprojects.rlbbview
  */
 
 import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.app.Activity
+import android.content.Context
+import android.view.MotionEvent
+import android.view.View
 
 val nodes : Int = 5
 val scGap : Float = 0.02f
@@ -19,3 +25,19 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+
+class RotatingLineBallBeaterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

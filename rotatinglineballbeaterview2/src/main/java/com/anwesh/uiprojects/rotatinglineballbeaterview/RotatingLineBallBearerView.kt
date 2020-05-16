3dcf613d31612196c1366ae705f7dd9124c7eb8c
Anwesh43 <anweshthecool0@gmail.com>
2020-05-17 02:43:57 +0530

    adding contants and libraries

--- rotatinglineballbeaterview2/src/main/java/com/anwesh/uiprojects/rotatinglineballbeaterview/RotatingLineBallBearerView.kt
+++ rotatinglineballbeaterview2/src/main/java/com/anwesh/uiprojects/rotatinglineballbeaterview/RotatingLineBallBearerView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.rotatinglineballbeaterview
  * Created by anweshmishra on 17/05/20.
  */
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+

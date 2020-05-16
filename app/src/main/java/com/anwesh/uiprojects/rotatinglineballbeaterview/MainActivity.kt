063090b066332ce472ba351f57f0f06a7a97a99b
Anwesh43 <anweshthecool0@gmail.com>
2020-05-17 03:28:59 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/rotatinglineballbeaterview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/rotatinglineballbeaterview/MainActivity.kt
@@ -2,10 +2,19 @@ package com.anwesh.uiprojects.rotatinglineballbeaterview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.rlbbview.RotatingLineBallBeaterView
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        RotatingLineBallBeaterView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
